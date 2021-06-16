34615b19121ab248b50b8311237fe9d38ffeead4
YCHX <ychx.alvin@gmail.com>
2021-06-16 15:59:08 +0900

    First commit

---
+++ Checklist.java
@@ -0,0 +1,10 @@
+public class Checklist {
+    String message;
+    boolean checked;
+
+    public Checklist(String message, boolean checked){
+        this.message = message;
+        this.checked = checked;
+    }
+    
+}
---
+++ Todo.java
@@ -0,0 +1,23 @@
+public class Todo {
+    String title;
+    String memo;
+    String tag;
+    String area;
+    boolean checked;
+    String[] deadline;
+
+    public Todo(String title){
+        this.title = title;
+        this.memo = "";
+        this.tag = "";
+        this.area = "";
+        this.checked = false;
+        this.deadline = new String[3];
+    }
+
+    public void setDeadline(String yyyy,String mm,String dd){
+        this.deadline[0] = yyyy;
+        this.deadline[1] = mm;
+        this.deadline[2] = dd;
+    }
+}
